10cd8602ffe3e1c839155295efb054d81c16a77e
Adomas Jackevičius <199406534+admjkv@users.noreply.github.com>
2025-02-23 20:30:09 +0100

    Refactor build-and-run script to improve directory handling and QEMU configuration

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
+set -e
 
-mkdir /minimos/efi_dir
-mkdir -p /minimos/efi_dir/EFI/BOOT
+rm -rf efi_dir
+mkdir -p efi_dir/EFI/BOOT
 
 nasm -f win64 bootx64.asm -o bootx64.obj
 lld-link /subsystem:efi_application /entry:_start /machine:x64 /out:BOOTx64.EFI bootx64.obj
 
-cp BOOTx64.EFI /minimos/efi_dir/EFI/BOOT/
+cp BOOTx64.EFI efi_dir/EFI/BOOT/
+cp /usr/share/edk2/x64/OVMF_VARS.4m.fd OVMF_VARS.fd
 
 qemu-system-x86_64 \
-  -enable-kvm \
-  -bios /usr/share/edk2/x64/OVMF_CODE.4m.fd \
-  -drive if=none,format=raw,file=fat:rw:/minimos/efi_dir,id=fs0 \
-  -device usb-storage,drive=fs0
+  -accel kvm \
+  -machine q35 \
+  -serial stdio \
+  -display none \
+  \
+  -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2/x64/OVMF_CODE.4m.fd \
+  -drive if=pflash,format=raw,file=OVMF_VARS.fd \
+  \
+  -drive file=fat:rw:efi_dir,format=raw,media=disk
