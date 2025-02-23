357b6267262e6ec419c24f1dd7f4f4bc014fcf6a
Adomas Jackevičius <199406534+admjkv@users.noreply.github.com>
2025-02-23 21:14:38 +0100

    Enhance cleanup script to remove additional EFI artifacts and directories

--- clean.sh
+++ clean.sh
@@ -3,4 +3,9 @@
 
 echo "Cleaning up build artifacts..."
 rm -f bootloader.bin kernel.bin os.img
+rm -f BOOTx64.EFI bootx64.obj OVMF_VARS.fd
+rm -f efi_dir/EFI/BOOT/BOOTx64.EFI
+rmdir efi_dir/EFI/BOOT
+rmdir efi_dir/EFI
+rmdir efi_dir
 echo "Cleanup complete."
