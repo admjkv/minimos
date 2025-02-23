c479ebd12e473ae03cb1e66f465a4120dc87efda
Adomas Jackevičius <199406534+admjkv@users.noreply.github.com>
2025-02-23 21:14:50 +0100

    Update build-and-run script for improved EFI application linking and OVMF_VARS path correction

--- build-and-run.sh
+++ build-and-run.sh
@@ -5,10 +5,10 @@ rm -rf efi_dir
 mkdir -p efi_dir/EFI/BOOT
 
 nasm -f win64 bootx64.asm -o bootx64.obj
-lld-link /subsystem:efi_application /entry:_start /machine:x64 /out:BOOTx64.EFI bootx64.obj
+lld-link /subsystem:EFI_APPLICATION /entry:_start /out:BOOTx64.EFI bootx64.obj
 
 cp BOOTx64.EFI efi_dir/EFI/BOOT/
-cp /usr/share/edk2/x64/OVMF_VARS.4m.fd OVMF_VARS.fd
+cp /usr/share/edk2-ovmf/x64/OVMF_VARS.4m.fd OVMF_VARS.fd
 
 qemu-system-x86_64 \
   -accel kvm \
@@ -16,10 +16,12 @@ qemu-system-x86_64 \
   -m 512M \
   -smp 1 \
   -machine pc \
-  -serial stdio \
-  -display none \
+  -display curses \
   \
   -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2/x64/OVMF_CODE.4m.fd \
   -drive if=pflash,format=raw,file=OVMF_VARS.fd \
   \
   -drive file=fat:rw:efi_dir,format=raw,media=disk
+
+rm -f BOOTx64.EFI bootx64.obj OVMF_VARS.fd
+rm -rf efi_dir
